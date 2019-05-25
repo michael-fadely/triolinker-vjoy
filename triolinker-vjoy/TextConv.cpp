0b73c8ac7bdc62af5979581cd32abc90387079db
Michael Fadely <michaelwfadely@gmail.com>
2019-05-24 20:52:11 -0600

    apply style to TextConv

--- triolinker-vjoy/TextConv.cpp
+++ triolinker-vjoy/TextConv.cpp
@@ -3,11 +3,14 @@
  * Text conversion functions.
  */
 
-#include "TextConv.hpp"
-#include <windows.h>
-
 #define WIN32_LEAN_AND_MEAN
-#define CP_SJIS 932
+#include <Windows.h>
+
+#include <string>
+
+#include "TextConv.hpp"
+
+constexpr auto CP_SJIS = 932;
 
 /**
  * Convert multibyte text to UTF-16.
@@ -15,12 +18,16 @@
  * @param cp Code page.
  * @return UTF-16 text (allocated via new[]), or nullptr on error.
  */
-wchar_t *MBStoUTF16(const char *mbs, unsigned int cp)
+wchar_t* MBStoUTF16(const char* mbs, unsigned int cp)
 {
-	int cchWcs = MultiByteToWideChar(cp, 0, mbs, -1, NULL, 0);
+	const int cchWcs = MultiByteToWideChar(cp, 0, mbs, -1, nullptr, 0);
+
 	if (cchWcs <= 0)
+	{
 		return nullptr;
-	wchar_t *wcs = new wchar_t[cchWcs];
+	}
+
+	const auto wcs = new wchar_t[cchWcs];
 	MultiByteToWideChar(cp, 0, mbs, -1, wcs, cchWcs);
 	return wcs;
 }
@@ -31,13 +38,19 @@ wchar_t *MBStoUTF16(const char *mbs, unsigned int cp)
  * @param cp Code page.
  * @return Multibyte text (allocated via new[]), or nullptr on error.
  */
-char *UTF16toMBS(const wchar_t *wcs, unsigned int cp)
+char* UTF16toMBS(const wchar_t* wcs, unsigned int cp)
 {
-	int cbMbs = WideCharToMultiByte(cp, 0, wcs, -1, NULL, 0, NULL, NULL);
+	const int cbMbs = WideCharToMultiByte(cp, 0, wcs, -1, nullptr,
+	                                      0, nullptr, nullptr);
+
 	if (cbMbs <= 0)
+	{
 		return nullptr;
-	char *mbs = new char[cbMbs];
-	WideCharToMultiByte(CP_UTF8, 0, wcs, -1, mbs, cbMbs, NULL, NULL);
+	}
+
+	const auto mbs = new char[cbMbs];
+	WideCharToMultiByte(CP_UTF8, 0, wcs, -1,
+	                    mbs, cbMbs, nullptr, nullptr);
 	return mbs;
 }
 
@@ -48,18 +61,24 @@ char *UTF16toMBS(const wchar_t *wcs, unsigned int cp)
  * @param sjis Shift-JIS text, null-terminated.
  * @return UTF-8 text (allocated via new[]), or nullptr on error.
  */
-char *SJIStoUTF8(const char *sjis)
+char* SJIStoUTF8(const char* sjis)
 {
 	// Convert from Shift-JIS to UTF-16.
-	wchar_t *wcs = MBStoUTF16(sjis, CP_SJIS);
+	wchar_t* wcs = MBStoUTF16(sjis, CP_SJIS);
+
 	if (!wcs)
+	{
 		return nullptr;
+	}
 
 	// Convert from UTF-16 to UTF-8.
-	char *mbs = UTF16toMBS(wcs, CP_UTF8);
+	char* mbs = UTF16toMBS(wcs, CP_UTF8);
 	delete[] wcs;
+
 	if (!mbs)
+	{
 		return nullptr;
+	}
 
 	return mbs;
 }
@@ -69,18 +88,24 @@ char *SJIStoUTF8(const char *sjis)
  * @param utf8 UTF-8 text, null-terminated.
  * @return Shift-JIS text (allocated via new[]), or nullptr on error.
  */
-char *UTF8toSJIS(const char *utf8)
+char* UTF8toSJIS(const char* utf8)
 {
 	// Convert from UTF-8 to UTF-16.
-	wchar_t *wcs = MBStoUTF16(utf8, CP_UTF8);
+	wchar_t* wcs = MBStoUTF16(utf8, CP_UTF8);
+
 	if (!wcs)
+	{
 		return nullptr;
+	}
 
 	// Convert from UTF-16 to Shift-JIS.
-	char *mbs = UTF16toMBS(wcs, CP_SJIS);
+	char* mbs = UTF16toMBS(wcs, CP_SJIS);
 	delete[] wcs;
+
 	if (!mbs)
+	{
 		return nullptr;
+	}
 
 	return mbs;
 }
@@ -90,27 +115,29 @@ char *UTF8toSJIS(const char *utf8)
  * @param utf8 UTF-8 text, null-terminated.
  * @return Windows-1252 text (allocated via new[]), or nullptr on error.
  */
-char *UTF8to1252(const char *utf8)
+char* UTF8to1252(const char* utf8)
 {
 	// Convert from UTF-8 to UTF-16.
-	wchar_t *wcs = MBStoUTF16(utf8, CP_UTF8);
+	wchar_t* wcs = MBStoUTF16(utf8, CP_UTF8);
+
 	if (!wcs)
+	{
 		return nullptr;
+	}
 
 	// Convert from UTF-16 to Shift-JIS.
-	char *mbs = UTF16toMBS(wcs, 1252);
+	char* mbs = UTF16toMBS(wcs, 1252);
 	delete[] wcs;
+
 	if (!mbs)
+	{
 		return nullptr;
+	}
 
 	return mbs;
 }
 
 /** C++ wrappers. **/
-
-#include <string>
-using std::string;
-using std::wstring;
 
 /**
  * Convert multibyte text to UTF-16.
@@ -118,13 +145,16 @@ using std::wstring;
  * @param cp Code page.
  * @return UTF-16 text, or empty string on error.
  */
-wstring MBStoUTF16(const string &mbs, unsigned int cp)
+std::wstring MBStoUTF16(const std::string& mbs, unsigned int cp)
 {
-	wchar_t *wcs = MBStoUTF16(mbs.c_str(), cp);
-	if (!wcs)
-		return wstring();
+	wchar_t* wcs = MBStoUTF16(mbs.c_str(), cp);
 
-	wstring wstr(wcs);
+	if (!wcs)
+	{
+		return std::wstring();
+	}
+
+	std::wstring wstr(wcs);
 	delete[] wcs;
 	return wstr;
 }
@@ -135,13 +165,16 @@ wstring MBStoUTF16(const string &mbs, unsigned int cp)
  * @param cp Code page.
  * @return Multibyte text, or empty string on error.
  */
-string UTF16toMBS(const wstring &wcs, unsigned int cp)
+std::string UTF16toMBS(const std::wstring& wcs, unsigned int cp)
 {
-	char *mbs = UTF16toMBS(wcs.c_str(), cp);
-	if (!mbs)
-		return string();
+	char* mbs = UTF16toMBS(wcs.c_str(), cp);
 
-	string mstr(mbs);
+	if (!mbs)
+	{
+		return std::string();
+	}
+
+	std::string mstr(mbs);
 	delete[] mbs;
 	return mstr;
 }
@@ -153,13 +186,16 @@ string UTF16toMBS(const wstring &wcs, unsigned int cp)
  * @param sjis Shift-JIS text.
  * @return UTF-8 text, or empty string on error.
  */
-string SJIStoUTF8(const string &sjis)
+std::string SJIStoUTF8(const std::string& sjis)
 {
-	char *utf8 = SJIStoUTF8(sjis.c_str());
-	if (!utf8)
-		return string();
+	char* utf8 = SJIStoUTF8(sjis.c_str());
 
-	string ustr(utf8);
+	if (!utf8)
+	{
+		return std::string();
+	}
+
+	std::string ustr(utf8);
 	delete[] utf8;
 	return ustr;
 }
@@ -169,13 +205,16 @@ string SJIStoUTF8(const string &sjis)
  * @param utf8 UTF-8 text.
  * @return Shift-JIS text, or empty string on error.
  */
-string UTF8toSJIS(const string &utf8)
+std::string UTF8toSJIS(const std::string& utf8)
 {
-	char *sjis = SJIStoUTF8(utf8.c_str());
-	if (!sjis)
-		return string();
+	char* sjis = SJIStoUTF8(utf8.c_str());
 
-	string jstr(sjis);
+	if (!sjis)
+	{
+		return std::string();
+	}
+
+	std::string jstr(sjis);
 	delete[] sjis;
 	return jstr;
 }
@@ -185,13 +224,16 @@ string UTF8toSJIS(const string &utf8)
  * @param utf8 UTF-8 text.
  * @return Windows-1252 text, or empty string on error.
  */
-string UTF8to1252(const string &utf8)
+std::string UTF8to1252(const std::string& utf8)
 {
-	char *w1252 = SJIStoUTF8(utf8.c_str());
-	if (!w1252)
-		return string();
+	char* w1252 = SJIStoUTF8(utf8.c_str());
 
-	string estr(w1252);
+	if (!w1252)
+	{
+		return std::string();
+	}
+
+	std::string estr(w1252);
 	delete[] w1252;
 	return estr;
 }
--- triolinker-vjoy/TextConv.hpp
+++ triolinker-vjoy/TextConv.hpp
@@ -6,54 +6,56 @@
 #ifndef TEXTCONV_HPP
 #define TEXTCONV_HPP
 
+#include <string>
+
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-/**
- * Convert multibyte text to UTF-16.
- * @param mbs Multibyte text, null-terminated.
- * @param cp Code page.
- * @return UTF-16 text (allocated via new[]), or nullptr on error.
- */
-wchar_t *MBStoUTF16(const char *mbs, unsigned int cp);
+	/**
+	 * Convert multibyte text to UTF-16.
+	 * @param mbs Multibyte text, null-terminated.
+	 * @param cp Code page.
+	 * @return UTF-16 text (allocated via new[]), or nullptr on error.
+	 */
+	wchar_t* MBStoUTF16(const char* mbs, unsigned int cp);
 
-/**
- * Convert UTF-16 text to multibyte.
- * @param wcs UTF-16 text, null-terminated.
- * @param cp Code page.
- * @return Multibyte text (allocated via new[]), or nullptr on error.
- */
-char *UTF16toMBS(const wchar_t *wcs, unsigned int cp);
+	/**
+	 * Convert UTF-16 text to multibyte.
+	 * @param wcs UTF-16 text, null-terminated.
+	 * @param cp Code page.
+	 * @return Multibyte text (allocated via new[]), or nullptr on error.
+	 */
+	char* UTF16toMBS(const wchar_t* wcs, unsigned int cp);
 
-/** Convenience functions. **/
+	/** Convenience functions. **/
 
-/**
- * Convert Shift-JIS text to UTF-8.
- * @param sjis Shift-JIS text, null-terminated.
- * @return UTF-8 text (allocated via new[]), or nullptr on error.
- */
-char *SJIStoUTF8(const char *sjis);
+	/**
+	 * Convert Shift-JIS text to UTF-8.
+	 * @param sjis Shift-JIS text, null-terminated.
+	 * @return UTF-8 text (allocated via new[]), or nullptr on error.
+	 */
+	char* SJIStoUTF8(const char* sjis);
 
-/**
- * Convert UTF-8 text to Shift-JIS.
- * @param utf8 UTF-8 text, null-terminated.
- * @return Shift-JIS text (allocated via new[]), or nullptr on error.
- */
-char *UTF8toSJIS(const char *utf8);
+	/**
+	 * Convert UTF-8 text to Shift-JIS.
+	 * @param utf8 UTF-8 text, null-terminated.
+	 * @return Shift-JIS text (allocated via new[]), or nullptr on error.
+	 */
+	char* UTF8toSJIS(const char* utf8);
 
-/**
- * Convert UTF-8 text to Windows-1252.
- * @param utf8 UTF-8 text, null-terminated.
- * @return Windows-1252 text (allocated via new[]), or nullptr on error.
- */
-char *UTF8to1252(const char *utf8);
+	/**
+	 * Convert UTF-8 text to Windows-1252.
+	 * @param utf8 UTF-8 text, null-terminated.
+	 * @return Windows-1252 text (allocated via new[]), or nullptr on error.
+	 */
+	char* UTF8to1252(const char* utf8);
 
 #ifdef __cplusplus
 }
 
 // C++ wrappers.
-#include <string>
 
 /**
  * Convert multibyte text to UTF-16.
@@ -61,7 +63,7 @@ char *UTF8to1252(const char *utf8);
  * @param cp Code page.
  * @return UTF-16 text, or empty string on error.
  */
-std::wstring MBStoUTF16(const std::string &mbs, unsigned int cp);
+std::wstring MBStoUTF16(const std::string& mbs, unsigned int cp);
 
 /**
  * Convert UTF-16 text to multibyte.
@@ -69,7 +71,7 @@ std::wstring MBStoUTF16(const std::string &mbs, unsigned int cp);
  * @param cp Code page.
  * @return Multibyte text, or empty string on error.
  */
-std::string UTF16toMBS(const std::wstring &wcs, unsigned int cp);
+std::string UTF16toMBS(const std::wstring& wcs, unsigned int cp);
 
 /** Convenience functions. **/
 
@@ -78,21 +80,21 @@ std::string UTF16toMBS(const std::wstring &wcs, unsigned int cp);
  * @param sjis Shift-JIS text.
  * @return UTF-8 text, or empty string on error.
  */
-std::string SJIStoUTF8(const std::string &sjis);
+std::string SJIStoUTF8(const std::string& sjis);
 
 /**
  * Convert UTF-8 text to Shift-JIS.
  * @param utf8 UTF-8 text.
  * @return Shift-JIS text, or empty string on error.
  */
-std::string UTF8toSJIS(const std::string &utf8);
+std::string UTF8toSJIS(const std::string& utf8);
 
 /**
  * Convert UTF-8 text to Windows-1252.
  * @param utf8 UTF-8 text.
  * @return Windows-1252 text, or empty string on error.
  */
-std::string UTF8to1252(const std::string &utf8);
+std::string UTF8to1252(const std::string& utf8);
 
 #endif /* __cplusplus */
 
